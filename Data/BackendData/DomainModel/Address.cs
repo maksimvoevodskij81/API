6b51e19aaa086526747596a48c3679a427ef0688
Maksym Voievodskyi <maksimvoevodskij81@gmail.com>
2023-02-02 19:44:54 +0100

    add Address Dto and DomainModel

---
+++ Data/BackendData/DomainModel/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace BackendData.DomainModel
+{
+    public class Address
+    {
+        public int AddressId { get; private set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+    }
+}
--- TaskAPI/ApiModels/AddressDto.cs
+++ TaskAPI/ApiModels/AddressDto.cs
@@ -4,11 +4,24 @@
     {
         public int AddressId { get; private set; }
         public string City { get; set; }
-        public string PostalCode { get; set; }
+        public string ZipCode { get; set; }
         public string Country { get; set; }
-        public string StreetLine { get; set; }
-        public string HouseNumber { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
 
+        public AddressDto()
+        {
+
+        }
+        public AddressDto(int addressId, string city, string zipCode, string country, string street, int houseNumber)
+        {
+            AddressId= addressId;
+            City= city;
+            ZipCode= zipCode;
+            Country= country;
+            Street= street;
+            HouseNumber = houseNumber;
+        }
 
     }
 }
