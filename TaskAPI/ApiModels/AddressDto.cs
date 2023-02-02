5b0dfc2a07d60eb515d1b780a53f292e43c7645a
Maksym Voievodskyi <maksimvoevodskij81@gmail.com>
2023-02-02 18:53:36 +0100

    Add project files.

---
+++ TaskAPI/ApiModels/AddressDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskAPI.ApiModels
+{
+    public class AddressDto
+    {
+        public int AddressId { get; private set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string StreetLine { get; set; }
+        public string HouseNumber { get; set; }
+
+
+    }
+}
